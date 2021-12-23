6deaad8fc89e296226fd12a12a7e65ac9cdcf678
Rodrigo Pongitor <rodrigo.pongitor@gmail.com>
2021-12-23 20:37:23 -0300

    Challenge Solved

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,31 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') return;
+
+    setTasks([...tasks, {
+      id: Math.floor(Math.random() * 1000000000),
+      title: newTaskTitle,
+      isComplete: false
+    }]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete
+        return task;
+      } else {
+        return task;
+      }
+    }))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter(task => task.id !== id));
   }
 
   return (
